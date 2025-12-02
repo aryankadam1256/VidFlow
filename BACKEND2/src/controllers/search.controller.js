605cbd1889fde3891df6d12aed5320cec4a5b4da
aryankadam1256 <kadamaryan425@gmail.com>
2025-12-02 15:11:25 +0530

    bug fixed realted to mongoose import

--- BACKEND2/src/controllers/search.controller.js
+++ BACKEND2/src/controllers/search.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Video from "../models/video.model.js";
 import User from "../models/user.model.js";
 import { Subscription } from "../models/subscription.model.js";
